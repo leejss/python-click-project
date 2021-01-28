4831dd96f3783a3d9f1d6cfbdbd863d67194353f
leejss <goldemsine@gmail.com>
2021-01-28 17:07:35 +0900

    feat: ADD max feature

--- sum-max_app/compute.py
+++ sum-max_app/compute.py
@@ -15,8 +15,22 @@ def compute_sum(ctx, param, value):
                     break
 
 
+def find_max(ctx, param, value):
+    if value:
+        click.echo("Enter 0 to exit")
+        max_number = 0
+        while True:
+            value = click.prompt("Enter Number: ", type=int)
+            max_number = max(max_number, value)
+            if value == 0:
+                if click.confirm("Do you want to exit? "):
+                    click.echo(f'Max: {max_number}')
+                    ctx.exit()
+                    break
+
+
 @click.command()
 @click.option('--sum', help="Compute sum and return", is_flag=True, callback=compute_sum, expose_value=False)
-@click.option('--max')
+@click.option('--max', help="Find max in given number", is_flag=True, callback=find_max, expose_value=False)
 def compute():
     pass
