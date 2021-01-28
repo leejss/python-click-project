4d9a777161f3ba74444fadfb2edc81c77645c9b3
leejss <goldemsine@gmail.com>
2021-01-28 16:54:40 +0900

    feat: ADD sum feature

--- sum-max_app/compute.py
+++ sum-max_app/compute.py
@@ -1,8 +1,21 @@
 import click
 
 
+def compute_sum(ctx, param, value):
+    if value:
+        click.echo("Enter 0 to exit")
+        total = 0
+        while True:
+            value = click.prompt("Enter Number: ", type=int)
+            total += value
+            if value == 0:
+                if click.confirm("Do you want to exit? "):
+                    ctx.exit()
+                    break
+
+
 @click.command()
-@click.option('--sum', help="Compute sum and return")
+@click.option('--sum', help="Compute sum and return", is_flag=True, callback=compute_sum, expose_value=False)
 @click.option('--max')
 def compute():
     pass
